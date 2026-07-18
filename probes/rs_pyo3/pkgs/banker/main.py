57d9fc27529a9363bb97c80ef1fd073f81992457
dark1zinn <dark1zinn00@gmail.com>
2026-07-18 19:33:18 -0300

    patch(rs_pyo3): comment out non included features in compiled binary

--- probes/rs_pyo3/pkgs/banker/main.py
+++ probes/rs_pyo3/pkgs/banker/main.py
@@ -6,17 +6,21 @@ app = typer.Typer()
 @app.command()
 def hello():
     print(greet.greet("Banker"))
-    exit(0)
+    # Seems that to allow certain features some flags should be set
+    # For the `exit()` function for example, seems to need `--no-deployment-flag=site-builtins`
+    # exit(0)
 
 @app.command()
 def pay(person: str, amount: int):
+    # The LSP may complain here since there are no signatures at all of this pay() function
+    # So despite it is there and it works, the LSP doesn't know about it
     print(greet.pay(amount, person))
-    exit(0)
+    # exit(0)
 
 @app.command()
 def deposit(amount: int):
     greet.deposit(amount)
-    exit(0)
+    # exit(0)
 
 if __name__ == "__main__":
     app()
