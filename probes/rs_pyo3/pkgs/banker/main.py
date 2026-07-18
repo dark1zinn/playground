3ba501f12dcdfd1166fbe3ee5aa5cf4e7e51d5fb
dark1zinn <dark1zinn00@gmail.com>
2026-07-18 15:16:27 -0300

    feat(rs_py03): transform banker script into a cli app

--- probes/rs_pyo3/pkgs/banker/main.py
+++ probes/rs_pyo3/pkgs/banker/main.py
@@ -1,7 +1,21 @@
-def main():
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def hello():
     print("Hello from banker!")
     exit(0)
 
+@app.command()
+def pay(person: str, amount: int):
+    print("You just paid " + str(amount) + " to " + str(person) + "!")
+    exit(0)
+
+@app.command()
+def deposit(amount: int):
+    print("You just deposited $" + str(amount))
+    exit(0)
 
 if __name__ == "__main__":
-    main()
+    app()
