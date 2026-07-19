d3d6cc9d61adfbeec8ac3629603a98387f8a79db
dark1zinn <dark1zinn00@gmail.com>
2026-07-19 10:09:21 -0300

    fix(rs_pyo3): explicitly import sys std package for use of exit() functions by nuitka

--- probes/rs_pyo3/pkgs/banker/main.py
+++ probes/rs_pyo3/pkgs/banker/main.py
@@ -1,5 +1,6 @@
 import typer
 import greet
+import sys
 
 from ui import ClockApp
 
@@ -13,20 +14,24 @@ def main():
 def hello():
     print(greet.greet("Banker"))
     # Seems that to allow certain features some flags should be set
-    # For the `exit()` function for example, seems to need `--no-deployment-flag=site-builtins`
-    # exit(0)
+    # for the `exit()` function for example, seems to need `--no-deployment-flag=site-builtins`
+    # ---
+    # EDIT: So apparently the problem is that just using plain `exit()` without importing from `sys`
+    # the GIL interpreter implictly knows about it, but for `nuitka` since there is no imports for it to follow
+    # it becomes an orphan fucntion (eg undefined function that doesn't exist at runtime)
+    sys.exit(0)
 
 @app.command()
 def pay(person: str, amount: int):
     # The LSP may complain here since there are no signatures at all of this pay() function
     # So despite it is there and it works, the LSP doesn't know about it
     print(greet.pay(amount, person))
-    # exit(0)
+    sys.exit(0)
 
 @app.command()
 def deposit(amount: int):
     greet.deposit(amount)
-    # exit(0)
+    sys.exit(0)
 
 if __name__ == "__main__":
     app()
